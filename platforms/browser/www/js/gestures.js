cb4c1fc409d4556e3f846decf3c12a151de985b1
santiagocadavid <santycadavid@hotmail.com>
2019-07-28 16:37:56 -0500

    proyecto emisora

--- platforms/browser/www/js/gestures.js
+++ platforms/browser/www/js/gestures.js
@@ -1,28 +1,82 @@
-var audio = new audio(emis);
+var emisora = document.getElementById('emisora');
+var emisoras = [ "http://20823.live.streamtheworld.com/RADIO_1_MED_SC?DIST=TuneIn&amp;TGT=TuneIn&amp;maxServers=2&amp;ua=RadioTime&amp;ttag=RadioTime",
+    "http://20823.live.streamtheworld.com/EL_SOL_MED_SC?DIST=TuneIn&amp;TGT=TuneIn&amp;maxServers=2&amp;ua=RadioTime&amp;ttag=RadioTime",
+    "http://198.27.68.65:8296/stream", "http://redpulsohd-server.info:9208", "http://65.60.45.74:8072/", "http://17643.live.streamtheworld.com:3690/BESAME_NACIONAL_SC?DIST=TuneIn&amp;TGT=TuneIn&amp;maxServers=2&amp;ua=RadioTime&amp;ttag=RadioTime",
+    "http://65.60.45.74:8070/;"];
+
+var app = {
+    // primera función que se ejecutar al cargar la pagina
+    inicio: function () {
+        this.iniciaBotones();
+        this.iniciaFastClick();
+        emisora.src = emisoras[0];
+    },
+    //inicia librería FastClick
+    iniciaFastClick: function () {
+        FastClick.attach(document.body);
+    },
+
+    iniciaBotones: function () {
+        var botonPlay = document.querySelector('#play');
+    
+        var botonPause = document.querySelector('#pausa');
+        var botonSig_emisora = document.querySelector('#Sig_emisora');
+        
+        var seleccion = document.querySelector("#radios");
+        //var botonVolMas = document.querySelector('#volumemas');
+        //var botonVolMenos = document.querySelector('#volumemenos');
+        var slider = document.querySelector("#myRange");
 
 
-var play = document.getElementById(play);
-play.addEventListener('click', function () {
-    audio.play;
-}, false);
+        botonPlay.addEventListener('click', app.play, false);
+        botonPause.addEventListener('click', app.pausa, false);
+        //botonVolMas.addEventListener('click', app.subeVol, false);
+        // botonVolMenos.addEventListener('click', app.bajaVol, false);        
+        slider.addEventListener('input', app.deslizar, false);
+        seleccion.addEventListener('change', app.cambiarEmisora, false);
 
-var pausa = document.getElementById(pausa)
-pausa.addEventListener('click', function(){
-    audio.pausa;
-}, false);
+    },
 
-var volumen = document.getElementById(volumenarriba)
-volumenarriba.addEventListener('change', function(){
-    audio.volumenarriba = parseFloat(this.value/10);
-
-},false);
-
-
-
-var volumen = document.getElementById(volumenabajo)
-volumenabajo.addEventListener('change', function(){
-    audio.volumenabajo = parseFloat(this.value/10);
-},false);
+    // función del botón play del reproductor
+    play: function () {
+        console.log("Play");
+        document.getElementById('emisora').play();
+    },
+    // función del botón pausa del reproductor
+    pausa: function () {
+        console.log("Pause");
+        document.getElementById('emisora').pause();
+    },
 
 
+    /*
+    // función del botón subir volume del reproductor
+    subeVol: function () {
+        document.getElementById('emisora').volume += 1.0;
+    },
+    // función del botón bajar volume del reproductor
+    bajaVol: function () {
+        document.getElementById('emisora').volume -= 0.1;
+    }*/
+    deslizar: function () {
+        console.log(this.value / 100)
+
+        document.getElementById('emisora').volume = this.value / 100;
+    },
+
+    cambiarEmisora: function(){
+        console.log(this.value);
+        app.pausa;
+        emisora.src = emisoras[this.value];
+
+    }
+
+};
+
+// inicializar al cargar la pagina
+if ('addEventListener' in document) {
+    document.addEventListener('DOMContentLoaded', function () {
+        app.inicio();
+    }, false);
+}
 
--- www/js/gestures.js
+++ www/js/gestures.js
@@ -1,21 +1,28 @@
 var emisora = document.getElementById('emisora');
+var emisoras = [ "http://20823.live.streamtheworld.com/RADIO_1_MED_SC?DIST=TuneIn&amp;TGT=TuneIn&amp;maxServers=2&amp;ua=RadioTime&amp;ttag=RadioTime",
+    "http://20823.live.streamtheworld.com/EL_SOL_MED_SC?DIST=TuneIn&amp;TGT=TuneIn&amp;maxServers=2&amp;ua=RadioTime&amp;ttag=RadioTime",
+    "http://198.27.68.65:8296/stream", "http://redpulsohd-server.info:9208", "http://65.60.45.74:8072/", "http://17643.live.streamtheworld.com:3690/BESAME_NACIONAL_SC?DIST=TuneIn&amp;TGT=TuneIn&amp;maxServers=2&amp;ua=RadioTime&amp;ttag=RadioTime",
+    "http://65.60.45.74:8070/;"];
 
 var app = {
     // primera función que se ejecutar al cargar la pagina
     inicio: function () {
         this.iniciaBotones();
         this.iniciaFastClick();
+        emisora.src = emisoras[0];
     },
     //inicia librería FastClick
     iniciaFastClick: function () {
         FastClick.attach(document.body);
     },
 
-    
     iniciaBotones: function () {
-
         var botonPlay = document.querySelector('#play');
+    
         var botonPause = document.querySelector('#pausa');
+        var botonSig_emisora = document.querySelector('#Sig_emisora');
+        
+        var seleccion = document.querySelector("#radios");
         //var botonVolMas = document.querySelector('#volumemas');
         //var botonVolMenos = document.querySelector('#volumemenos');
         var slider = document.querySelector("#myRange");
@@ -26,22 +33,22 @@ var app = {
         //botonVolMas.addEventListener('click', app.subeVol, false);
         // botonVolMenos.addEventListener('click', app.bajaVol, false);        
         slider.addEventListener('input', app.deslizar, false);
-        
-        },
-        
-        
-    
+        seleccion.addEventListener('change', app.cambiarEmisora, false);
 
-    iniciaHammer: function () {
     },
+
     // función del botón play del reproductor
     play: function () {
+        console.log("Play");
         document.getElementById('emisora').play();
     },
     // función del botón pausa del reproductor
     pausa: function () {
+        console.log("Pause");
         document.getElementById('emisora').pause();
     },
+
+
     /*
     // función del botón subir volume del reproductor
     subeVol: function () {
@@ -51,13 +58,18 @@ var app = {
     bajaVol: function () {
         document.getElementById('emisora').volume -= 0.1;
     }*/
-    deslizar: function(){
+    deslizar: function () {
+        console.log(this.value / 100)
 
-        console.log(this.value/100)
-        
-        document.getElementById('emisora').volume = this.value/100;
-        
-        }
+        document.getElementById('emisora').volume = this.value / 100;
+    },
+
+    cambiarEmisora: function(){
+        console.log(this.value);
+        app.pausa;
+        emisora.src = emisoras[this.value];
+
+    }
 
 };
 
